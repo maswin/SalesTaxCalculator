ac3f8a739fdbb9b24f6e48b851aea2a6931c5054
alagapm <alagapm@INalagapm.local>
2015-12-16 15:17:52 +0530

    Initial Commit

---
+++ src/SalesTaxCalculator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by alagapm on 12/16/15.
+ */
+public class SalesTaxCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
